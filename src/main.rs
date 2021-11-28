65a962361a9de8a7dd470a870ec7b4a682bbc4ca
Julien CLEMENT <julien.clement@epita.fr>
2021-11-28 02:12:32 +0100

    feat(bootloader): add bootable image
    Signed-off-by: Julien CLEMENT <julien.clement@epita.fr>

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,21 @@ fn panic(_info: &core::panic::PanicInfo) -> !
     loop {}
 }
 
+static HELLO: &[u8] = b"Welcome to the JuliOS";
+
 #[no_mangle]
 pub extern "C" fn _start() -> !
 {
+    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate()
+    {
+        unsafe
+        {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
