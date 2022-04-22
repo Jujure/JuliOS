204b464f59de9dbfae905cf72d8af8d5499bd316
Julien CLEMENT <julien.clement@epita.fr>
2022-04-22 08:55:26 +0200

    fix(heap): increased heap size to 1MiB
    Signed-off-by: Julien CLEMENT <julien.clement@epita.fr>

--- src/memory/heap_alloc/mod.rs
+++ src/memory/heap_alloc/mod.rs
@@ -2,7 +2,7 @@ use super::PAGE_SIZE;
 use linked_list_allocator::LockedHeap;
 
 pub const HEAP_START: u64 = 0x4444_4444_0000;
-pub const HEAP_SIZE: u64 = PAGE_SIZE as u64 * 25;
+pub const HEAP_SIZE: u64 = PAGE_SIZE as u64 * 256;
 
 #[global_allocator]
 pub static ALLOCATOR: LockedHeap = LockedHeap::empty();
