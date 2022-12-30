bc0c885052787237db4da8c64ec1eefe92e3fb73
Julien CLEMENT <julien.clement@epita.fr>
2022-12-31 00:05:17 +0100

    add memory allocation for thread stack
    Signed-off-by: Julien CLEMENT <julien.clement@epita.fr>

--- src/proc/thread/mod.rs
+++ src/proc/thread/mod.rs
@@ -1,9 +1,17 @@
+use alloc::alloc::{alloc, dealloc, Layout};
+
+const STACK_SIZE: usize = 4096 * 20;
+
 pub struct Thread {
     rsp: u64
 }
 
 impl Thread {
-    pub fn new() {
-
+    pub fn new() -> Self {
+        unsafe {
+            Thread {
+                rsp: alloc(Layout::new::<[u8; STACK_SIZE]>()) as u64,
+            }
+        }
     }
 }
